fdccc01113ef2365d8ac9bc70c619a89d787ce51
Jan Phillips <jan.phillips.82@planninginspectorate.gov.uk>
2026-03-24 14:44:46 +0000

    chore: removed old PoC (LP-585)

--- apps/portal/src/util/config-middleware.ts
+++ apps/portal/src/util/config-middleware.ts
@@ -19,7 +19,7 @@ export function addLocalsConfiguration(): Handler {
 		];
 
 		res.locals.config = {
-			styleFile: 'style-9ac0aae2.css',
+			styleFile: 'style-3747c019.css',
 			cspNonce: res.locals.cspNonce,
 			headerTitle: 'A New Service',
 			footerLinks: [
