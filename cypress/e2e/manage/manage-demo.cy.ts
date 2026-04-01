b79f46592d9f31a2d3c4f3ac09384922859bd028
VarshaPINS <Varsha.Patel.9a@planninginspectorate.gov.uk>
2026-04-01 12:00:24 +0100

    feat(LP-592): add e2e pipeline YAML for local test execution

--- cypress/e2e/manage/manage-demo.cy.ts
+++ cypress/e2e/manage/manage-demo.cy.ts
@@ -5,7 +5,6 @@
 describe('Manage Demo Tests', () => {
 	it('should load the manage homepage', { tags: ['smoke'] }, () => {
 		cy.visit('/');
-		cy.log('CYPRESS TESTS+111');
 		//manageHomePage.verifyMainHeading('Some Service Name');
 	});
 });
--- cypress/e2e/portal/portal-demo.cy.ts
+++ cypress/e2e/portal/portal-demo.cy.ts
@@ -4,8 +4,6 @@
 describe('Portal Demo Tests', () => {
 	it('should load the portal homepage', { tags: ['smoke'] }, () => {
 		cy.visit('/');
-		cy.log('CYPRESS TESTS+222');
-
 		// homePage.verifyHeading('Local Plans Examination Service');
 		// homePage.verifyDbConnection('Successfully connected to the database.');
 	});
