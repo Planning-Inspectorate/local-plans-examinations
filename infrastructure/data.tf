4b6ea2c3e8121c14c249fc3c157d58df11e16608
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-04-27 14:35:02 +0100

    chore(infrastructure): adding private endpoint for SQL storage accounts

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -48,6 +48,13 @@ data "azurerm_private_dns_zone" "keyvault" {
   provider = azurerm.tooling
 }
 
+data "azurerm_private_dns_zone" "storage" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
+
 data "azurerm_virtual_network" "tooling" {
   name                = var.tooling_config.network_name
   resource_group_name = var.tooling_config.network_rg
