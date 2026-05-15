d81325fffb38fe34a78abeffe99b1f94ff6bdb7f
Thomas Pike <tpike1999@icloud.com>
2026-05-15 13:49:31 +0100

    feat(LP-637): landing page added

--- apps/portal/src/app/router.ts
+++ apps/portal/src/app/router.ts
@@ -2,6 +2,7 @@ import { Router as createRouter } from 'express';
 import { cacheNoCacheMiddleware } from '@pins/local-plans-lib/middleware/cache.ts';
 import { createErrorRoutes } from './views/static/error/index.ts';
 import { createHomeRoutes } from './views/home/index.ts';
+import { createLandingPageRoutes } from './views/landingPage/index.ts';
 import { createMonitoringRoutes } from '@pins/local-plans-lib/controllers/monitoring.ts';
 import type { PortalService } from '#service';
 import type { IRouter } from 'express';
@@ -22,7 +23,7 @@ export function buildRouter(service: PortalService): IRouter {
 	router.use(cacheNoCacheMiddleware);
 
 	router.use('/login', createLoginRoutes(service));
-
+	router.use('/landingPage', createLandingPageRoutes(service));
 	router.use('/', createHomeRoutes(service));
 	router.use('/error', createErrorRoutes(service));
 
---
+++ apps/portal/src/app/views/landingPage/controller.ts
@@ -0,0 +1,65 @@
+import type { PortalService } from '#service';
+import type { AsyncRequestHandler } from '@pins/local-plans-lib/util/async-handler.ts';
+import fs from 'node:fs'; //added assume ok?ß
+import { StageLabel } from '../../types.ts';
+import type { Plan } from '../../types.ts';
+import { StatusColour } from '../../types.ts';
+
+/**
+ * Example home page controller
+ */
+export function buildLandingPage(service: PortalService): AsyncRequestHandler {
+	const { logger } = service;
+	return async (req, res) => {
+		const councilLocation = 'Southampton City Council'; //TO BE CHANGED
+		const rawPlans = JSON.parse(fs.readFileSync('src/app/testData.json', 'utf-8'));
+
+		//filters and validates plans
+		//---------currently just removes inccorect or incomplete records ----------
+		const validPlans: Plan[] = rawPlans.filter((rawPlan: unknown): rawPlan is Plan => {
+			if (typeof rawPlan !== 'object' || rawPlan === null) return false;
+			const recordPlan = rawPlan as Record<string, unknown>;
+			return (
+				typeof recordPlan.refNum === 'string' &&
+				typeof recordPlan.leadLPA === 'string' &&
+				typeof recordPlan.title === 'string' &&
+				typeof recordPlan.stage === 'number' &&
+				typeof recordPlan.status === 'number' &&
+				recordPlan.stage >= 0 &&
+				recordPlan.stage <= 3 &&
+				recordPlan.status >= 0 &&
+				recordPlan.status <= 5
+			);
+		});
+
+		//maps tags to their classes and the text
+		const mappedPlans = validPlans.map((plan) => [
+			{ html: `<a class="govuk-link" href="/planPage/${plan.refNum.replace('/', '-')}">${plan.refNum}</a>` },
+			{ text: plan.leadLPA },
+			{ text: plan.title },
+			{ text: StageLabel[plan.stage] },
+			{
+				html: (() => {
+					const s = StatusColour[plan.status];
+					return `<strong class=" ${s?.class ?? ''}">
+					${s?.label ?? 'Unknown'}
+					</strong>`;
+				})()
+			}
+		]);
+
+		//content
+		const viewModel = {
+			plans: mappedPlans
+		};
+
+		logger.info({ viewModel }, 'landing page');
+
+		//headings
+		return res.render('views/landingPage/view.njk', {
+			pageCaption: councilLocation,
+			pageTitle: 'My plans',
+			...viewModel
+		});
+	};
+}
