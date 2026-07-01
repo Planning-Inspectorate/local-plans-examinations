655f5d8f00ca5342f66f09cf84d9fc7f2a866575
Eoin Corr <Eoin.Corr.5e@planninginspectorate.gov.uk>
2026-07-01 11:34:11 +0100

    test: fix case overview format and nest it in correct folder structure

--- cypress/page-objects/manage/home-page.ts
+++ cypress/page-objects/manage/home-page.ts
@@ -9,12 +9,28 @@ export class ManageHomePage extends BasePage {
 		return cy.getByData('create-a-case');
 	}
 
+	get noCasesMessage() {
+		return cy.getByData('no-cases');
+	}
+
+	get casesTable() {
+		return cy.getByData('casesTable');
+	}
+
 	startCreateCase() {
 		this.createCaseLink.should('be.visible').click();
 	}
 
+	openCaseByPlanTitle(planTitle: string) {
+		this.casesTable.contains('tr', planTitle).find('a').first().click();
+	}
+
 	verifyCreateCaseLink(text: string) {
 		this.createCaseLink.should('be.visible').and('contain.text', text);
+	}
+
+	verifyNoCasesMessage(text: string) {
+		this.noCasesMessage.should('be.visible').and('contain.text', text);
 	}
 }
 
