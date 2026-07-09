4889b5cb1385d619dfb461745310054ee5228392
CSallis-hmcts <christopher.sallis@solirius.com>
2026-07-09 08:56:21 +0100

    test: improve portal validation tests

---
+++ cypress/constants/portal/error-messages.ts
@@ -0,0 +1,19 @@
+export const ERROR_MESSAGES = {
+	// Login
+	NO_EMAIL_ENTERED: 'Enter your email address',
+	NO_EMAIL_ENTERED_SUMMARY: 'You have not entered your email address',
+
+	INVALID_EMAIL_FORMAT: 'Enter the valid email address your reference number was sent to',
+	INVALID_EMAIL_FORMAT_SUMMARY: 'Enter a valid email address',
+
+	UNRECOGNISED_EMAIL: 'Enter an email address linked to a case on this service',
+	UNRECOGNISED_EMAIL_SUMMARY: 'We did not recognise that email address',
+
+	// OPT
+
+	NO_CODE_ENTERED_SUMMARY: 'You have not entered a code',
+	NO_CODE_ENTERED: 'Enter the code we sent to your email address',
+
+	INCORRECT_CODE_SUMMARY: 'The code you entered is incorrect'
+	// INCORRECT_CODE: '';
+};
