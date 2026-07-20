9355b101b930a405365fdcd13a0aa89150111612
Christopher-Sallis <christopher.sallis@solirius.com>
2026-07-20 09:28:47 +0100

    test: add gateway 2 submission page tests

---
+++ cypress/e2e/portal/gw2-application/journey/gateway-2-application.cy.ts
@@ -0,0 +1,32 @@
+import { portalLogin } from '../../../../flows/portal/login-flow.ts';
+import type { PlanDetailsFixture } from '../../../../fixtures/portal/types.ts';
+import { gateway2ApplicationPage } from '../../../../page-objects/portal/gw2-application/gateway-2-application-page.ts';
+import { planDetailsPage } from '../../../../page-objects/portal/plan-details/plan-details-page.ts';
+
+const loadPlanDetails = () => cy.fixture<PlanDetailsFixture>('portal/plan-details.json');
+
+describe('Gateway 2 application page journeys', () => {
+	beforeEach(() => {
+		portalLogin();
+	});
+
+	it('Navigates to Plan Details page when the Back link is clicked', { tags: ['smoke'] }, () => {
+		loadPlanDetails().then((plan) => {
+			gateway2ApplicationPage.visit(plan.urlReference);
+			gateway2ApplicationPage.verifyLoaded();
+
+			gateway2ApplicationPage.backLink.click();
+			planDetailsPage.verifyPathForPlan(plan.urlReference);
+		});
+	});
+
+	it('Navigates to Plan Details page when the Save and come back later link is clicked', { tags: ['smoke'] }, () => {
+		loadPlanDetails().then((plan) => {
+			gateway2ApplicationPage.visit(plan.urlReference);
+			gateway2ApplicationPage.verifyLoaded();
+
+			gateway2ApplicationPage.saveAndComeBackLink.click();
+			planDetailsPage.verifyPathForPlan(plan.urlReference);
+		});
+	});
+});
