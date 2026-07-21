ed2a3a0449461227966e33b01231dafc2db1f1e1
Jan Phillips <jan.phillips.82@planninginspectorate.gov.uk>
2026-07-21 15:36:50 +0100

    fix: adding TEMPLATE_ID to Terraform

--- infrastructure/app-manage.tf
+++ infrastructure/app-manage.tf
@@ -62,6 +62,7 @@ module "app_manage" {
     SQL_CONNECTION_STRING    = local.key_vault_refs["sql-app-connection-string"]
     GOV_NOTIFY_API_KEY       = local.key_vault_refs["localplans-gov-notify-api-key"]
     GOV_NOTIFY_WEBHOOK_TOKEN = local.key_vault_refs["gov-notify-webhook-token"]
+    TEMPLATE_ID              = local.key_vault_refs["template-id"]
 
     # retries
     RETRY_MAX_ATTEMPTS = "3"
