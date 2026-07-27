42a6b73410e2385f266d357283d7c31957819b57
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-07-27 11:57:20 +0100

    feat: use managed redis cluster

--- apps/manage/src/app/config.ts
+++ apps/manage/src/app/config.ts
@@ -76,8 +76,8 @@ export function loadConfig(): Config {
 		GIT_SHA,
 		LOG_LEVEL,
 		PORT,
+		MANAGED_REDIS_URL,
 		NODE_ENV,
-		REDIS_CONNECTION_STRING,
 		SESSION_SECRET,
 		SQL_CONNECTION_STRING,
 		GOV_NOTIFY_DISABLED,
@@ -169,7 +169,7 @@ export function loadConfig(): Config {
 		srcDir: buildConfig.srcDir,
 		session: {
 			redisPrefix: 'manage:',
-			redis: REDIS_CONNECTION_STRING,
+			redis: MANAGED_REDIS_URL,
 			secret: SESSION_SECRET
 		},
 		// the static directory to serve assets from (images, css, etc..)
