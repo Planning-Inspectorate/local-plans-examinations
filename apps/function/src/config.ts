8c3d599136c4f274217f281bbdaf8724ec78a61f
Dave Ferguson <dave.ferguson.g3@planninginspectorate.gov.uk>
2026-07-30 16:54:11 +0100

    feat(LP-724): removed example function

--- apps/function/src/config.ts
+++ apps/function/src/config.ts
@@ -2,10 +2,6 @@ import { loadEnvFile } from 'node:process';
 import type { DatabaseConfig } from '@pins/local-plans-lib/app/config-types.d.ts';
 
 export interface Config {
-	example: {
-		enabled: boolean;
-		schedule: string;
-	};
 	database: DatabaseConfig;
 }
 
@@ -15,17 +11,13 @@ export function loadConfig(): Config {
 	try { loadEnvFile(); } catch {/* ignore errors*/}
 
 	// get values from the environment
-	const { EXAMPLE_FUNCTION_ENABLED, EXAMPLE_SCHEDULE, SQL_CONNECTION_STRING } = process.env;
+	const { SQL_CONNECTION_STRING } = process.env;
 
 	if (!SQL_CONNECTION_STRING) {
 		throw new Error('SQL_CONNECTION_STRING is required');
 	}
 
 	return {
-		example: {
-			enabled: EXAMPLE_FUNCTION_ENABLED === 'true',
-			schedule: EXAMPLE_SCHEDULE || '0 0 0 * * *' // default to daily at midnight
-		},
 		database: {
 			connectionString: SQL_CONNECTION_STRING
 		}
