3653f19b5dae44250286abeff5f2d3ad537c1b22
Thomas Pike <tpike1999@icloud.com>
2026-07-31 14:57:16 +0100

    feat: added delete page and deletedDate and added filtering to stop deleted cases displaying

---
+++ packages/database/src/migrations/20260731121708_setup/migration.sql
@@ -0,0 +1,22 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- DropForeignKey
+ALTER TABLE [dbo].[CaseHistory] DROP CONSTRAINT [CaseHistory_caseId_fkey];
+
+-- AddForeignKey
+ALTER TABLE [dbo].[CaseHistory] ADD CONSTRAINT [CaseHistory_caseId_fkey] FOREIGN KEY ([caseId]) REFERENCES [dbo].[Case]([id]) ON DELETE CASCADE ON UPDATE CASCADE;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
