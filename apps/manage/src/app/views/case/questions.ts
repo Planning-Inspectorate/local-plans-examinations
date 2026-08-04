ecc76ecb38e4549594fa8ba84b6c1810dfe9e9a3
Kieran Smith <kieransmith63@gmail.com>
2026-08-04 09:43:46 +0100

    feat: change fields to auto completes and provide values

--- apps/manage/src/app/views/case/questions.ts
+++ apps/manage/src/app/views/case/questions.ts
@@ -197,8 +197,15 @@ const caseQuestions: Record<string, ManageQuestionConfig> = {
 		inputAttributes: { 'data-cy': 'gateway-2-assessor' }
 	},
 	assessorGateway3: {
-		type: COMPONENT_TYPES.SINGLE_LINE_INPUT,
-		question: 'Who is the Assessor Gateway 3?',
+		type: COMPONENT_TYPES.SELECT,
+		question: 'Who is the Gateway 3 assessor?',
+		options: [
+			{ value: '', text: '' },
+			{ value: 'assessor-1', text: 'Assessor 1' },
+			{ value: 'assessor-2', text: 'Assessor 2' },
+			{ value: 'assessor-3', text: 'Assessor 3' },
+			{ value: 'assessor-4', text: 'Assessor 4' }
+		],
 		fieldName: 'assessorGateway3',
 		url: 'assessor-gateway-3',
 		title: 'Assessor Gateway 3',
@@ -220,40 +227,75 @@ const caseQuestions: Record<string, ManageQuestionConfig> = {
 		validators: [new RequiredValidator('Input Examining Inspector 1')]
 	},
 	examiningInspector2: {
-		type: COMPONENT_TYPES.SINGLE_LINE_INPUT,
+		type: COMPONENT_TYPES.SELECT,
 		question: 'Which Inspector is assigned for Examination?',
+		options: [
+			{ value: '', text: '' },
+			{ value: 'inspector-1', text: 'Inspector 1' },
+			{ value: 'inspector-2', text: 'Inspector 2' },
+			{ value: 'inspector-3', text: 'Inspector 3' },
+			{ value: 'inspector-4', text: 'Inspector 4' }
+		],
 		fieldName: 'examiningInspector2',
 		url: 'examining-inspector-2',
 		title: 'Examining Inspector 2',
 		validators: [new RequiredValidator('Input Examining Inspector 2')]
 	},
 	examiningInspector3: {
-		type: COMPONENT_TYPES.SINGLE_LINE_INPUT,
+		type: COMPONENT_TYPES.SELECT,
 		question: 'Which Inspector is assigned for Examination?',
+		options: [
+			{ value: '', text: '' },
+			{ value: 'inspector-1', text: 'Inspector 1' },
+			{ value: 'inspector-2', text: 'Inspector 2' },
+			{ value: 'inspector-3', text: 'Inspector 3' },
+			{ value: 'inspector-4', text: 'Inspector 4' }
+		],
 		fieldName: 'examiningInspector3',
 		url: 'examining-inspector-3',
 		title: 'Examining Inspector 3',
 		validators: [new RequiredValidator('Input Examining Inspector 3')]
 	},
 	qaInspector1: {
-		type: COMPONENT_TYPES.SINGLE_LINE_INPUT,
+		type: COMPONENT_TYPES.SELECT,
 		question: 'Which Inspector is assigned for QA?',
+		options: [
+			{ value: '', text: '' },
+			{ value: 'inspector-1', text: 'Inspector 1' },
+			{ value: 'inspector-2', text: 'Inspector 2' },
+			{ value: 'inspector-3', text: 'Inspector 3' },
+			{ value: 'inspector-4', text: 'Inspector 4' }
+		],
 		fieldName: 'qaInspector1',
 		url: 'qa-inspector-1',
 		title: 'QA Inspector 1',
 		validators: [new RequiredValidator('Input QA Inspector 1')]
 	},
 	qaInspector2: {
-		type: COMPONENT_TYPES.SINGLE_LINE_INPUT,
+		type: COMPONENT_TYPES.SELECT,
 		question: 'Which Inspector is assigned for QA?',
+		options: [
+			{ value: '', text: '' },
+			{ value: 'inspector-1', text: 'Inspector 1' },
+			{ value: 'inspector-2', text: 'Inspector 2' },
+			{ value: 'inspector-3', text: 'Inspector 3' },
+			{ value: 'inspector-4', text: 'Inspector 4' }
+		],
 		fieldName: 'qaInspector2',
 		url: 'qa-inspector-2',
 		title: 'QA Inspector 2',
 		validators: [new RequiredValidator('Input QA Inspector 2')]
 	},
 	qaInspector3: {
-		type: COMPONENT_TYPES.SINGLE_LINE_INPUT,
+		type: COMPONENT_TYPES.SELECT,
 		question: 'Which Inspector is assigned for QA?',
+		options: [
+			{ value: '', text: '' },
+			{ value: 'inspector-1', text: 'Inspector 1' },
+			{ value: 'inspector-2', text: 'Inspector 2' },
+			{ value: 'inspector-3', text: 'Inspector 3' },
+			{ value: 'inspector-4', text: 'Inspector 4' }
+		],
 		fieldName: 'qaInspector3',
 		url: 'qa-inspector-3',
 		title: 'QA Inspector 3',
