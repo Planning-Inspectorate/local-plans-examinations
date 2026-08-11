522374181db1554e250b8b9e0a5e8efdd410611b
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-08-11 10:30:49 +0100

    fix: incorrect field name

--- packages/database/src/seed/seed-cy.ts
+++ packages/database/src/seed/seed-cy.ts
@@ -40,7 +40,7 @@ async function run() {
 				gateway1Date: now,
 				gateway2Date: now,
 				gateway3Date: now,
-				estimatedSubmissionDate: now,
+				estimatedSubmissionForExaminationDate: now,
 				lpas: {
 					connectOrCreate: lpaCodes.map((lpaCode) => ({
 						where: { lpaCode },
