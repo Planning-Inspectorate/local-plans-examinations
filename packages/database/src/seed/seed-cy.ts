9179794150ba15bdf7b2258995ed739f45df3009
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-08-11 11:07:41 +0100

    fix: incorrect field reference

--- packages/database/src/seed/seed-cy.ts
+++ packages/database/src/seed/seed-cy.ts
@@ -40,7 +40,6 @@ async function run() {
 				gateway1Date: now,
 				gateway2Date: now,
 				gateway3Date: now,
-				estimatedSubmissionForExaminationDate: now,
 				lpas: {
 					connectOrCreate: lpaCodes.map((lpaCode) => ({
 						where: { lpaCode },
@@ -86,6 +85,7 @@ async function run() {
 				},
 				examinationInfo: {
 					create: {
+						estimatedSubmissionForExaminationDate: now,
 						submissionForExaminationDate: now,
 						letterSentToMHCLGDate: new Date('2026-10-01T12:00:00.000Z'),
 						letterIssueDate: new Date('2026-11-01T12:00:00.000Z'),
