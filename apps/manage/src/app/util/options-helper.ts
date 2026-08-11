3b96f466da961df25f892db4f373b382c2a1226b
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-08-11 09:40:46 +0100

    feat: dynamically generate inspector options

--- apps/manage/src/app/util/options-helper.ts
+++ apps/manage/src/app/util/options-helper.ts
@@ -21,3 +21,22 @@ export function buildCaseOfficerOptions(service: ManageService, questions: Recor
 		next();
 	});
 }
+
+export function buildInspectorOptions(service: ManageService, questions: Record<string, any>) {
+	return asyncHandler(async (req: Request, _res: Response, next: NextFunction) => {
+		const entraClient = service.getEntraClient(req.session as authSession.SessionWithAuth);
+
+		if (service.authDisabled) {
+			next();
+			return;
+		}
+
+		const inspectors = entraClient ? await entraClient.listAllGroupMembers(service.entraGroupIds.inspectors) : [];
+
+		const options_map = [{ value: '', text: '' }, ...inspectors.map((m) => ({ value: m.id, text: m.displayName }))];
+		questions.examiningInspector1.options = options_map;
+		questions.examiningInspector2.options = options_map;
+		questions.examiningInspector3.options = options_map;
+		next();
+	});
+}
