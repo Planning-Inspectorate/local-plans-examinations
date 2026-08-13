9825d3eb8bd4699dff4d7d4ba83199ba48210476
Conor Morgan <conor.morgan@solirius.com>
2026-08-13 15:09:55 +0100

    test: rudamentary journey test for delete case

--- cypress/page-objects/manage/case-overview/delete-case-page.ts
+++ cypress/page-objects/manage/case-overview/delete-case-page.ts
@@ -4,6 +4,37 @@ export class DeletCasePage extends BasePage {
 	constructor() {
 		super(/^\/case\/.+\/delete-case$/);
 	}
+
+	get deleteCaseButton() {
+		return cy.getByData('confirm-delete-button');
+	}
+
+	get table() {
+		return cy.getByData('delete-case-details');
+	}
+
+	verifyLoaded() {
+		super.verifyLoaded();
+		this.verifyHeading('Are you sure you want to delete this case?');
+		this.verifyMainContains(
+			'This will mark the case as deleted. It will no longer appear in the system but will still be available for audit purposes.',
+			'Case details'
+		);
+		this.verifyFixtureCaseData();
+	}
+
+	verifyFixtureCaseData() {
+		this.table.within(() => {
+			cy.get('.govuk-table__row').should('contain.text', 'Cypress Test Plan');
+			cy.get('.govuk-table__row').should('contain.text', 'local-plan');
+			cy.get('.govuk-table__row').should('contain.text', 'lpa-1', 'lpa-2');
+			cy.get('.govuk-table__row').should('contain.text', 'officer-1');
+		});
+	}
+
+	deleteCase() {
+		this.deleteCaseButton.click();
+	}
 }
 
 export const deletCasePage = new DeletCasePage();
