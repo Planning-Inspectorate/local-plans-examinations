26d94fc644a224eacf3672fe602ee65f5e47b719
Kieran Smith <kieransmith63@gmail.com>
2026-08-20 15:02:48 +0100

    fix: keep just one file for seed and read of lpas

--- packages/database/src/seed/seed-dev.ts
+++ packages/database/src/seed/seed-dev.ts
@@ -2,7 +2,6 @@ import { newDatabaseClient } from '../index.ts';
 import { seedStaticData } from './data-static.ts';
 import { seedDev } from './data-dev.ts';
 import { loadConfig } from '../configuration/config.ts';
-import { seedDevAuthorities } from './data-authorities-dev.ts';
 
 async function run() {
 	const config = loadConfig();
@@ -11,7 +10,6 @@ async function run() {
 
 	try {
 		await seedStaticData(dbClient);
-		await seedDevAuthorities(dbClient);
 		await seedDev(dbClient);
 	} catch (error) {
 		console.error(error);
