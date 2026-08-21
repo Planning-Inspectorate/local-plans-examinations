9b00160a7f037818fbc7c7dec7a99a02a5bc8360
Sonia Choudhury <55599714+itsmesonia@users.noreply.github.com>
2026-08-21 15:30:25 +0100

    feat(portal): lp-671 scoping consultation documents upload page

--- apps/portal/src/app/views/manage-local-plans/gateway-2-submission/questions.ts
+++ apps/portal/src/app/views/manage-local-plans/gateway-2-submission/questions.ts
@@ -158,11 +158,38 @@ export const draftStatementOfComplianceQuestion: FileUploaderQuestionProps = {
 	validators: []
 };
 
+export const scopingConsultationDocumentsQuestion: FileUploaderQuestionProps = {
+	type: CUSTOM_COMPONENTS.FILE_UPLOADER,
+	title: 'Scoping consultation documents',
+	question: 'Upload your scoping consultation documents',
+	fieldName: 'scopingConsultationDocuments',
+	url: 'scoping-cons',
+	allowedFileExtensions: MINIMAL_PROCEDURAL_ALLOWED_EXTENSIONS,
+	allowedMimeTypes: MINIMAL_PROCEDURAL_ALLOWED_MIME_TYPES,
+	maxFileSizeBytes: SINGLE_FILE_UPLOAD_LIMIT,
+	maxFileSizeLabel: SINGLE_FILE_UPLOAD_LIMIT_LABEL,
+	maxFilesPerUpload: UNLIMITED_FILES,
+	maxTotalUploadSizeBytes: TOTAL_FILE_UPLOAD_LIMIT,
+	maxTotalUploadSizeLabel: TOTAL_FILE_UPLOAD_LIMIT_LABEL,
+	multiple: true,
+	text: {
+		caption: 'Consultation documents',
+		introduction: 'Drag and drop or choose files',
+		fileRequirementsText:
+			'Each file must be a PDF, DOC, DOCX, PPT, PPTX, XLS, XLSX, MSG, JPG, JPEG, PNG, TIF or TIFF and smaller than 250MB. The total size of your uploaded files must be smaller than 1GB.',
+		chooseFilesButtonText: 'Choose files',
+		dropInstructionText: 'or drop files',
+		continueButtonText: 'Save and return'
+	},
+	validators: []
+};
+
 export const gateway2FileUploadQuestions = {
 	gateway2CoverLetter: gateway2CoverLetterQuestion,
 	localPlanTimetable: localPlanTimetableQuestion,
 	projectInitiationDocument: projectInitiationDocumentQuestion,
-	draftStatementOfCompliance: draftStatementOfComplianceQuestion
+	draftStatementOfCompliance: draftStatementOfComplianceQuestion,
+	scopingConsultationDocuments: scopingConsultationDocumentsQuestion
 } satisfies Record<string, CrownQuestionProps>;
 
 export const questions = createQuestions(
