28fa88c8caabf03d77df1d7a3dcf25f4a32f8460
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-09-01 15:24:11 +0100

    chore: improve commenting

--- apps/manage/src/app/views/case/submission-check/submission-check-factory.ts
+++ apps/manage/src/app/views/case/submission-check/submission-check-factory.ts
@@ -22,6 +22,16 @@ const OPTIONS: Record<
 	'gateway-3-document': Gateway3SubmissionCheck
 };
 
+/**
+ * Factory function for SubmissionCheck, which can be used to dynamically generate a SubmissionCheck object by question
+ *
+ * ### Example usage
+ * ```
+ * const submissionCheckClass = getSubmissionCheckForQuestion('some-question');
+ * const submissionCheck = new submissionCheckClass(...);
+ * const submissionCheckData = await submissionCheck.generateDataForPage();
+ * ```
+ */
 export function getSubmissionCheckForQuestion(question: string) {
 	if (question in OPTIONS) {
 		return OPTIONS[question];
