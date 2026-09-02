5507c26968eb6478a51ec62376dc678105fd48bd
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-09-02 13:33:00 +0100

    chore: urlencode the url when redirecting

--- apps/manage/src/app/views/case/controller.ts
+++ apps/manage/src/app/views/case/controller.ts
@@ -1084,7 +1084,7 @@ export function issueGateway2Report(service: ManageService, journeyId: string): 
 			req.session.alertMessage = 'Gateway 2 report already issued';
 			req.session.alertMessageStatus = 'important';
 		}
-		res.redirect(`/case/${encodeURIComponent(caseReference)}/${journeyId}`);
+		res.redirect(`/case/${encodeURIComponent(caseReference)}/${encodeURIComponent(journeyId)}`);
 		return;
 	};
 }
@@ -1136,7 +1136,7 @@ export function issueGateway1SLA(service: ManageService, journeyId: string): Asy
 			req.session.alertMessage = 'SLA already issued';
 			req.session.alertMessageStatus = 'important';
 		}
-		res.redirect(`/case/${encodeURIComponent(caseReference)}/${journeyId}`);
+		res.redirect(`/case/${encodeURIComponent(caseReference)}/${encodeURIComponent(journeyId)}`);
 		return;
 	};
 }
@@ -1188,7 +1188,7 @@ export function issueGateway3Document(service: ManageService, journeyId: string)
 			req.session.alertMessage = 'Gateway 3 decision already issued';
 			req.session.alertMessageStatus = 'important';
 		}
-		res.redirect(`/case/${encodeURIComponent(caseReference)}/${journeyId}`);
+		res.redirect(`/case/${encodeURIComponent(caseReference)}/${encodeURIComponent(journeyId)}`);
 		return;
 	};
 }
