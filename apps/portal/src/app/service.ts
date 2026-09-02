08f0e6e70dbce80fb67be6bcc1240e392cbcfed4
Dave Ferguson <dave.ferguson.g3@planninginspectorate.gov.uk>
2026-09-02 14:58:10 +0100

    chore(portal): clarity setup

--- apps/portal/src/app/service.ts
+++ apps/portal/src/app/service.ts
@@ -7,12 +7,14 @@ import { buildTestPlans } from './types.ts';
 export class PortalService extends BaseService {
 	readonly auth: Config['auth'];
 	readonly environment: Config['environment'];
+	readonly clarityId: string | undefined;
 	readonly notifyClient: GovNotifyClient | null;
 
 	constructor(config: Config) {
 		super(config);
 		this.auth = config.auth;
 		this.environment = config.environment;
+		this.clarityId = config.clarityId;
 		this.notifyClient = initGovNotify(config.govNotify, this.logger);
 	}
 
