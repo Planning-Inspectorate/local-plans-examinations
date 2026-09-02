126015d63ecace85aa07f8fff8b63cc8c25861df
Christopher-Sallis <christopher.sallis@solirius.com>
2026-09-02 14:22:47 +0100

    test: udpated cross-service test to pass in CI

--- cypress/flows/portal/login-flow.ts
+++ cypress/flows/portal/login-flow.ts
@@ -25,12 +25,16 @@ export const portalLogin = () => {
 
 export const manageToPortalLogin = () => {
 	cy.task('seedCase');
+
+	cy.origin(Cypress.env('portalBaseUrl'), { args: { email: TEST_EMAIL } }, ({ email }) => {
+		cy.visit('/login');
+		cy.setCookie('cookie_consent', 'accept');
+		cy.get('[data-cy="email"]').clear().type(email);
+		cy.get('[data-cy="button-save-and-continue"]').click();
+	});
+
 	cy.task('seedOtp').then((otp) => {
-		cy.origin(Cypress.env('portalBaseUrl'), { args: { email: TEST_EMAIL, otp: String(otp) } }, ({ email, otp }) => {
-			cy.visit('/login');
-			cy.setCookie('cookie_consent', 'accept');
-			cy.get('[data-cy="email"]').clear().type(email);
-			cy.get('[data-cy="button-save-and-continue"]').click();
+		cy.origin(Cypress.env('portalBaseUrl'), { args: { otp: String(otp) } }, ({ otp }) => {
 			cy.get('[data-cy="otp"]').clear().type(otp);
 			cy.get('[data-cy="button-save-and-continue"]').click();
 		});
