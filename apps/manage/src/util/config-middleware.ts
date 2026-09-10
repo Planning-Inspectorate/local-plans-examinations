c031a7529913f2b2f12908d02162c462b1d79783
Kieran Smith <kieransmith63@gmail.com>
2026-09-10 11:24:33 +0100

    fix: linting

--- apps/manage/src/util/config-middleware.ts
+++ apps/manage/src/util/config-middleware.ts
@@ -6,7 +6,7 @@ import type { Handler } from 'express';
 export function addLocalsConfiguration(): Handler {
 	return (req, res, next) => {
 		res.locals.config = {
-			styleFile: 'style-ebf989f8.css',
+			styleFile: 'style-2c8a7818.css',
 			headerTitle: 'Plans Examination back office'
 		};
 		next();
