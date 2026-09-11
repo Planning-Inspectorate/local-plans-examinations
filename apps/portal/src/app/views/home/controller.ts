37a0af709b8cb29cbe23a8afb718fabc7a2d7146
Jan Phillips <jan.phillips.82@planninginspectorate.gov.uk>
2026-09-11 16:29:14 +0100

    feat: guidance is a protected page; using GDS components instead of classes (LP-746)

--- apps/portal/src/app/views/home/controller.ts
+++ apps/portal/src/app/views/home/controller.ts
@@ -33,6 +33,8 @@ export function buildHomePage(service: PortalService): AsyncRequestHandler {
 
 export function buildGuidancePage(): AsyncRequestHandler {
 	return async (req, res) => {
-		return res.render('views/home/guidance.njk');
+		return res.render('views/home/guidance.njk', {
+			pageTitle: 'Guidance page'
+		});
 	};
 }
--- apps/portal/src/app/views/home/index.ts
+++ apps/portal/src/app/views/home/index.ts
@@ -3,13 +3,14 @@ import { buildGuidancePage, buildHomePage } from './controller.ts';
 import { asyncHandler } from '@planning-inspectorate/core/util';
 import type { PortalService } from '#service';
 import type { IRouter } from 'express';
+import { checkIsAuthenticated } from '../../auth/guards.ts';
 
 export function createHomeRoutes(service: PortalService): IRouter {
 	const router = createRouter({ mergeParams: true });
 
 	const homePageController = buildHomePage(service);
 	router.get('/', asyncHandler(homePageController));
-	router.get('/guidance', asyncHandler(buildGuidancePage()));
+	router.get('/guidance', checkIsAuthenticated, asyncHandler(buildGuidancePage()));
 
 	return router;
 }
