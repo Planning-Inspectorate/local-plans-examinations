9787c83b4c65bc5f1c400fd2eb01117863da2c3e
Jan Phillips <jan.phillips.82@planninginspectorate.gov.uk>
2026-09-16 16:53:48 +0100

    test: unit tests for exposeAuthToViews; using session.destroy() on logout (LP746 787)

--- apps/portal/src/app/router.ts
+++ apps/portal/src/app/router.ts
@@ -29,8 +29,9 @@ export function buildRouter(service: PortalService): IRouter {
 	router.use(exposeAuthToViews);
 	router.use('/login', createLoginRoutes(service));
 	router.use('/logout', (req, res) => {
-		req.session.isAuthenticated = false;
-		res.redirect('/login');
+		req.session.destroy(() => {
+			res.redirect('/login');
+		});
 	});
 	router.use('/landingPage', createLandingPageRoutes(service));
 	router.use('/manage-local-plans', manageLocalPlansRoutes(service));
