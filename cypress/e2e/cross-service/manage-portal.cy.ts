c788e87261c75322f452a83c0b092ed9596d06c7
Christopher-Sallis <christopher.sallis@solirius.com>
2026-09-17 11:29:22 +0100

    test: work in progress for doc upload

--- cypress/e2e/cross-service/manage-portal.cy.ts
+++ cypress/e2e/cross-service/manage-portal.cy.ts
@@ -9,11 +9,7 @@ import { gateway2ApplicationPage } from '../../page-objects/portal/gw2-applicati
 import { manageToPortalLogin } from '../../flows/portal/login-flow.ts';
 import { manageHomePage } from '../../page-objects/manage/home-page.ts';
 //import { seededCase } from '../../fixtures/manage/case.ts';
-import type { PlanDetailsFixture } from '../../fixtures/portal/types.ts';
-import { gateway2CoverLetterPage } from '../../page-objects/portal/gw2-application/gateway-2-uploads.page.ts';
-import { openGateway2DocumentUploadPage } from '../../flows/portal/gateway-2-upload-flow.ts';
-
-const loadPlanDetails = () => cy.fixture<PlanDetailsFixture>('portal/plan-details.json');
+import { gateway2UploadAnswers } from 'cypress/fixtures/portal/gateway-2-uploads.ts';
 
 export const CREATE_CASE_TEST_EMAIL = 'test@planninginspectorate.gov.uk';
 export const FIXTURE_TEST_EMAIL = 'cypress@test.com';
@@ -44,19 +40,35 @@ describe('Cross-service Manage and Portal', () => {
 		});
 	});
 
-	it('displays files uploaded to the FO in the BO GW2 Submission Documents', () => {
+	it.only('displays files uploaded to the FO in the BO GW2 Submission Documents', () => {
 		manageHomePage.visit();
 		manageHomePage.getReference().then((caseReference) => {
 			const portalPlanReference = toPortalPlanReference(caseReference);
 			manageToPortalLogin(FIXTURE_TEST_EMAIL);
 
-			const page = gateway2CoverLetterPage;
-			loadPlanDetails().then((plan) => {
-				const newPlan: PlanDetailsFixture = {
-					...plan,
-					reference: portalPlanReference
-				};
-				openGateway2DocumentUploadPage(newPlan, page);
+			const doc = gateway2UploadAnswers.coveringLetter;
+			const fileName = 'test-document.pdf';
+
+			cy.fixture(`files/${fileName}`, 'base64').then((base64) => {
+				cy.origin(
+					Cypress.env('portalBaseUrl'),
+					{ args: { portalPlanReference, fileName, base64, doc } },
+					({ portalPlanReference, fileName, base64, doc }) => {
+						cy.visit(`/manage-local-plans/${portalPlanReference}/gateway-2-submission`);
+						cy.get(`[data-cy="${doc.addCy}"]`).should('be.visible').click();
+
+						const binary = Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
+
+						cy.get(`#${doc.fieldName}-input`).selectFile(
+							{ contents: binary, fileName, mimeType: 'application/pdf' },
+							{ force: true }
+						);
+						cy.get('[data-cy="upload-files-button"]').click();
+						cy.get('.govuk-summary-list__value').should('contain', 'test-document.pdf');
+
+						cy.get('[data-cy="save-and-return-button"]').click();
+					}
+				);
 			});
 		});
 		//file upload journeys
