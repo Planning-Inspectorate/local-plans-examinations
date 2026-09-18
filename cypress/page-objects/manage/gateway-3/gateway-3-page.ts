82b0b040999bc9a086007d612a38c527db169797
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-09-18 15:29:52 +0100

    fix: update regex patterns

--- cypress/page-objects/manage/gateway-3/gateway-3-page.ts
+++ cypress/page-objects/manage/gateway-3/gateway-3-page.ts
@@ -31,7 +31,10 @@ const actionLinkHrefs: Array<[string, RegExp]> = [
 		new RegExp(`^/case/.+/gateway-3/gateway-3/${gateway3ProgrammeOfficerAnswer.path}$`)
 	],
 	[gateway3ExaminationWebsite.row, new RegExp(`^/case/.+/gateway-3/gateway-3/${gateway3ExaminationWebsite.path}$`)],
-	[gateway3DecisionAnswer.row, new RegExp(`^/case/.+/gateway-3/gateway-3-submission/${gateway3DecisionAnswer.path}$`)]
+	[
+		gateway3DecisionAnswer.row,
+		new RegExp(`^/case/.+/gateway-3/gateway-3-submission-(\\d)+/${gateway3DecisionAnswer.path}-(\\d)+$`)
+	]
 ];
 
 export class Gateway3Page extends GatewayBasePage {
@@ -67,17 +70,17 @@ export class Gateway3Page extends GatewayBasePage {
 		this.verifySummaryRowContains(gateway3DocumentsAnswer.row, '2 documents');
 		this.verifySummaryRowActionHref(
 			gateway3DocumentsAnswer.row,
-			new RegExp(`^/case/.+/gateway-3/gateway-3-submission/${gateway3DocumentsAnswer.path}$`)
+			new RegExp(`^/case/.+/gateway-3/gateway-3-submission-(\\d)+/${gateway3DocumentsAnswer.path}-(\\d)+$`)
 		);
 		this.summaryRowActionLink(gateway3DocumentsAnswer.row).should('contain.text', 'View');
 		this.verifySummaryRowActionHref(
 			gateway3DecisionAnswer.row,
-			new RegExp(`^/case/.+/gateway-3/gateway-3-submission/${gateway3DocumentsAnswer.path}/check$`)
+			new RegExp(`^/case/.+/gateway-3/gateway-3-submission-(\\d)+/${gateway3DocumentsAnswer.path}-(\\d)+/check$`)
 		);
 		this.summaryRowActionLink(gateway3DecisionAnswer.row).should('contain.text', 'View');
 		this.verifySummaryRowActionHref(
 			gateway3CompletionDate.row,
-			new RegExp(`^/case/.+/gateway-3/gateway-3-submission/${gateway3CompletionDate.path}$`)
+			new RegExp(`^/case/.+/gateway-3/gateway-3-submission-(\\d)+/${gateway3CompletionDate.path}-(\\d)+$`)
 		);
 		this.summaryRowActionLink(gateway3CompletionDate.row).should('contain.text', 'View');
 	}
