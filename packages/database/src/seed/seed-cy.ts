4b2d97d2c441498efb2996514d0f47a07c3606b6
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-09-18 10:31:20 +0100

    chore: seed gateway3 info

--- packages/database/src/seed/seed-cy.ts
+++ packages/database/src/seed/seed-cy.ts
@@ -99,7 +99,17 @@ async function run() {
 						assessorAppointmentDate: new Date('2026-09-01T12:00:00.000Z'),
 						programmeOfficerFirstName: 'Albert',
 						programmeOfficerLastName: 'Einstien',
-						programmeOfficerEmail: 'gateway3.officer@test.com'
+						programmeOfficerEmail: 'gateway3.officer@test.com',
+						submission: {
+							createMany: {
+								data: [
+									{
+										decision: undefined,
+										completionDate: undefined
+									}
+								]
+							}
+						}
 					}
 				},
 				examinationInfo: {
