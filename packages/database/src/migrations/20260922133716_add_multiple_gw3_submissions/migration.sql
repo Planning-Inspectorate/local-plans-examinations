f191173da62c67a7cc1523db17c5fadcefd448c9
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-09-22 14:40:14 +0100

    chore: update migration for submissions

--- packages/database/src/migrations/20260922133716_add_multiple_gw3_submissions/migration.sql
+++ packages/database/src/migrations/20260922133716_add_multiple_gw3_submissions/migration.sql
@@ -9,6 +9,12 @@ BEGIN TRY
 
 BEGIN TRAN;
 
+-- DropForeignKey
+ALTER TABLE [dbo].[Gateway3Info] DROP CONSTRAINT [Gateway3Info_decision_fkey];
+
+-- DropIndex
+DROP INDEX [Gateway3Info_decision_idx] ON [dbo].[Gateway3Info];
+
 -- AlterTable
 ALTER TABLE [dbo].[Gateway3Info] DROP COLUMN [completionDate],
 [decision];
@@ -16,7 +22,7 @@ ALTER TABLE [dbo].[Gateway3Info] DROP COLUMN [completionDate],
 -- CreateTable
 CREATE TABLE [dbo].[Gateway3Submission] (
     [id] UNIQUEIDENTIFIER NOT NULL,
-    [decision] NVARCHAR(1000),
+    [decision] NVARCHAR(50),
     [completionDate] DATETIME2,
     [gateway3InfoId] UNIQUEIDENTIFIER,
     CONSTRAINT [Gateway3Submission_pkey] PRIMARY KEY CLUSTERED ([id])
@@ -24,6 +30,9 @@ CREATE TABLE [dbo].[Gateway3Submission] (
 
 -- AddForeignKey
 ALTER TABLE [dbo].[Gateway3Submission] ADD CONSTRAINT [Gateway3Submission_gateway3InfoId_fkey] FOREIGN KEY ([gateway3InfoId]) REFERENCES [dbo].[Gateway3Info]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Gateway3Submission] ADD CONSTRAINT [Gateway3Submission_decision_fkey] FOREIGN KEY ([decision]) REFERENCES [dbo].[Gateway3Decision]([id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 COMMIT TRAN;
 
